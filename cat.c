d4d8735ce8154e87ca0ebec63d9aee2879f38e2d
Isaac Johnson <isaac.johnson2@griffithuni.edu.au>
2014-03-10 09:31:37 +1000

    Added no argument functionality to cat.c

--- cat.c
+++ cat.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-#define BLOCK_SIZE 512
+#define BLOCK_SIZE 255
 
 int main(int argc, char *argv[])
 {
-	FILE *filePath;
-	char fileName[BLOCK_SIZE], ch;
-	int i;
+	char ch;
 
-	for (i = 1; i < argc; i++) {
-		strncpy(fileName, argv[i], BLOCK_SIZE);
-		filePath = fopen(fileName, "r");
-		if (filePath == NULL) {
-			printf("%s: No such file or directory\n", fileName);
-			continue;
-		}		
+	if (argc > 1) {
+		FILE *filePath;
+		char fileName[BLOCK_SIZE];
+		int i;
+
+		for (i = 1; i < argc; i++) {
+			strncpy(fileName, argv[i], BLOCK_SIZE);
+			filePath = fopen(fileName, "r");
+			if (filePath == NULL) {
+				printf("%s: No such file or directory\n", fileName);
+				continue;
+			}		
 	
-		while ((ch = fgetc(filePath)) != EOF) {
-			putchar(ch);
+			while ((ch = fgetc(filePath)) != EOF) {
+				fputc(ch, stdout);
+			}
+
+			fclose(filePath);
 		}
-
-		fclose(filePath);
+	} else {
+		while ((ch = fgetc(stdin)) != EOF) {
+			fputc(ch, stdout);
+		}
 	}
-
 	return 0;
 }

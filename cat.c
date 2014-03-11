0d7a6a485a25371cde55df50523ea2a8f32e0dc5
Isaac Johnson <isaac.johnson2@griffithuni.edu.au>
2014-03-12 07:05:48 +1000

    Added additional commenting to cat.c

--- cat.c
+++ cat.c
@@ -2,15 +2,20 @@
 #include <string.h>
 #define BLOCK_SIZE 255
 
+/* 
+* A simple cat command program that emulates the unix cat command.
+* Ability to take command line arguments and display data appropriately
+*/
 int main(int argc, char *argv[])
 {
+	// helper variables
 	char ch;
+	int i;
 	
-	// if command line arguments exist
+	// if command line arguments exist (only support for file names)
 	if (argc > 1) {
 		FILE *filePath;
 		char fileName[BLOCK_SIZE];
-		int i;
 
 		for (i = 1; i < argc; i++) {
 			strncpy(fileName, argv[i], BLOCK_SIZE);
@@ -20,7 +25,7 @@ int main(int argc, char *argv[])
 				continue;
 			}		
 			
-			// write file contents to stdout
+			//  write file contents to stdout
 			while ((ch = fgetc(filePath)) != EOF) {
 				fputc(ch, stdout);
 			}
@@ -28,7 +33,7 @@ int main(int argc, char *argv[])
 			fclose(filePath);
 		}
 	} else {
-		// if no command line arguments exist
+		// if no command line arguments exist (no file name specified)
 		while ((ch = fgetc(stdin)) != EOF) {
 			fputc(ch, stdout);
 		}
